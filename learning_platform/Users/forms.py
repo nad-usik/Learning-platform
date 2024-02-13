901e26297ab482b95c7fd93fd9e4d27f688ef252
nad-usik <124509955+nad-usik@users.noreply.github.com>
2024-02-13 14:16:53 +0300

    feat: create forms for Users app

--- learning_platform/Users/forms.py
+++ learning_platform/Users/forms.py
@@ -1,34 +1,97 @@
-from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+# from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 from django import forms
+from .models import CustomUser, Profile
+
+
+# from django.core.exceptions import ValidationError
+
 
 class SignUpForm(UserCreationForm):
-    email = forms.EmailField(label="", widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Email Address'}))
-    first_name = forms.CharField(label="", max_length=100, widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'First Name'}))
-    last_name = forms.CharField(label="", max_length=100, widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Last Name'}))
+    email = forms.EmailField(label="",
+                             widget=forms.TextInput(
+                                 attrs={'class': 'form-control', 'placeholder': 'Электронная почта'}))
+    role = forms.CharField(label='', widget=forms.Select(choices=CustomUser.ROLE_CHOICES,
+                                                         attrs={'class': 'form-control'}))
+    password1 = forms.CharField(label="",
+                                widget=forms.PasswordInput(
+                                    attrs={'class': 'form-control', 'placeholder': 'Введите пароль'}))
+    password2 = forms.CharField(label="",
+                                widget=forms.PasswordInput(
+                                    attrs={'class': 'form-control', 'placeholder': 'Подтвердите пароль'}))
 
     class Meta:
-        model = User
-        fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2')
+        model = CustomUser
+        fields = ('email', 'role', 'password1', 'password2')
 
-    def __init__(self, *args, **kwargs):
-        super(SignUpForm, self).__init__(*args, **kwargs)
+    # def __init__(self, *args, **kwargs):
+    #     super(SignUpForm, self).__init__(*args, **kwargs)
+    #
+    #     self.fields['password1'].widget.attrs['class'] = 'form-control'
+    #     self.fields['password1'].widget.attrs['placeholder'] = 'Password'
+    #     self.fields['password1'].label = ''
+    #     self.fields[
+    #         'password1'].help_text = '<ul class="form-text text-muted small"><li>Your password can\'t be too similar to your other personal information.</li><li>Your password must contain at least 8 characters.</li><li>Your password can\'t be a commonly used password.</li><li>Your password can\'t be entirely numeric.</li></ul>'
+    #
+    #     self.fields['password2'].widget.attrs['class'] = 'form-control'
+    #     self.fields['password2'].widget.attrs['placeholder'] = 'Confirm Password'
+    #     self.fields['password2'].label = ''
+    #     self.fields[
+    #         'password2'].help_text = '<span class="form-text text-muted"><small>Enter the same password as before, for verification.</small></span>'
 
-        self.fields['username'].widget.attrs['class'] = 'form-control'
-        self.fields['username'].widget.attrs['placeholder'] = 'User Name'
-        self.fields['username'].label = ''
-        self.fields['username'].help_text = '<span class="form-text text-muted"><small>Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.</small></span>'
 
-        self.fields['password1'].widget.attrs['class'] = 'form-control'
-        self.fields['password1'].widget.attrs['placeholder'] = 'Password'
-        self.fields['password1'].label = ''
-        self.fields['password1'].help_text = '<ul class="form-text text-muted small"><li>Your password can\'t be too similar to your other personal information.</li><li>Your password must contain at least 8 characters.</li><li>Your password can\'t be a commonly used password.</li><li>Your password can\'t be entirely numeric.</li></ul>'
+class ProfileForm(forms.ModelForm):
+    last_name = forms.CharField(label='', max_length=50,
+                                widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Фамилия'}))
+    first_name = forms.CharField(label='', max_length=50,
+                                 widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Имя'}))
+    second_name = forms.CharField(label='', max_length=50,
+                                  widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Отчество'}),
+                                  required=False)
+    date_of_birth = forms.DateField(label='', widget=forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}))
+    gender = forms.CharField(label='', widget=forms.Select(choices=Profile.GENDER_CHOICES,
+                                                           attrs={'class': 'form-control', 'placeholder': 'Пол'}))
+    town = forms.CharField(label='', widget=forms.TextInput(attrs={'placeholder': 'Город', 'class': 'form-control'}))
+    phone_number = forms.CharField(label='', widget=forms.TextInput(
+        attrs={'placeholder': 'Номер Телефона', 'class': 'form-control'}))
+    profile_photo = forms.ImageField(label='',
+                                     widget=forms.FileInput(attrs={'class': 'form-control', 'id': 'inputGroupFile01'}),
+                                     required=False)
 
-        self.fields['password2'].widget.attrs['class'] = 'form-control'
-        self.fields['password2'].widget.attrs['placeholder'] = 'Confirm Password'
-        self.fields['password2'].label = ''
-        self.fields['password2'].help_text = '<span class="form-text text-muted"><small>Enter the same password as before, for verification.</small></span>'
+    class Meta:
+        model = Profile
+        fields = (
+            'last_name', 'first_name', 'second_name', 'date_of_birth', 'gender', 'town', 'phone_number',
+            'profile_photo')
 
-class EmailLoginForm(AuthenticationForm):
-    email = forms.EmailField(widget=forms.TextInput(attrs={'class': 'form-input'}))
-    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-input'}))
+
+class EditProfileForm(forms.ModelForm):
+    last_name = forms.CharField(label='', max_length=50,
+                                widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Фамилия'}))
+    first_name = forms.CharField(label='', max_length=50,
+                                 widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Имя'}))
+    second_name = forms.CharField(label='', max_length=50,
+                                  widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Отчество'}),
+                                  required=False)
+    date_of_birth = forms.DateField(label='', widget=forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}))
+    gender = forms.CharField(label='', widget=forms.Select(choices=Profile.GENDER_CHOICES,
+                                                           attrs={'class': 'form-control', 'placeholder': 'Пол'}))
+    town = forms.CharField(label='', widget=forms.TextInput(attrs={'placeholder': 'Город', 'class': 'form-control'}))
+    phone_number = forms.CharField(label='', widget=forms.TextInput(
+        attrs={'placeholder': 'Номер Телефона', 'class': 'form-control'}))
+    profile_photo = forms.ImageField(label='',
+                                     widget=forms.FileInput(attrs={'class': 'form-control', 'id': 'inputGroupFile01'}),
+                                     required=False)
+
+    class Meta:
+        model = Profile
+        fields = (
+            'last_name', 'first_name', 'second_name', 'date_of_birth', 'gender', 'town', 'phone_number',
+            'profile_photo')
+
+
+class EmailLoginForm(forms.Form):
+    email = forms.EmailField(
+        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Введите Электронную Почту'}))
+    password = forms.CharField(
+        widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Введите Пароль', 'cols': '12'}))
