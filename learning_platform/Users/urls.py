b502ce948edce06c06d16c10d89aa0f36cf5d934
nad-usik <124509955+nad-usik@users.noreply.github.com>
2024-02-13 14:20:55 +0300

    feat: add urls for create, edit and view views

--- learning_platform/Users/urls.py
+++ learning_platform/Users/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from User.views import *
+from Users.views import *
 
 urlpatterns = [
     path('login/', login_user, name='login'),
     path('logout/', logout_user, name='logout'),
     path('register/', register_user, name='register'),
+    path('create_profile/', create_profile, name='create_profile'),
+    path('edit_profile/', edit_profile, name='edit_profile'),
+    path('profile/', view_profile, name='profile'),
 ]
