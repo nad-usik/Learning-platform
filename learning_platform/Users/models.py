367d1a1e28912ec5de80da3e154ede08dde1b74b
nad-usik <124509955+nad-usik@users.noreply.github.com>
2024-02-13 14:18:04 +0300

    fix: update models for Users app

--- learning_platform/Users/models.py
+++ learning_platform/Users/models.py
@@ -1,10 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-from django.db.models.signals import post_save
+
+
+# from django.db.models.signals import post_save
 
 # Registrate a user
 class CustomUser(AbstractUser):
+    username = models.CharField(max_length=100, unique=False, null=True)
     email = models.EmailField(max_length=100, unique=True)
+    password = models.CharField(max_length=150)
     ROLE_CHOICES = [
         ('teacher', 'Преподаватель'),
         ('student', 'Ученик'),
@@ -12,16 +16,17 @@ class CustomUser(AbstractUser):
     role = models.CharField(max_length=10, choices=ROLE_CHOICES)
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']
-    is_staff = models.BooleanField(default=True)
-    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f'{self.email}, {self.role}'
 
 
 # Fill in user's profile
 class Profile(models.Model):
-    profile_id = models.AutoField(primary_key=True)
-    user_id = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
-    surname = models.CharField(max_length=50)
-    name = models.CharField(max_length=50)
+    # profile_id = models.AutoField(primary_key=True)
+    user = models.OneToOneField("CustomUser", on_delete=models.CASCADE)
+    last_name = models.CharField(max_length=50)
+    first_name = models.CharField(max_length=50)
     second_name = models.CharField(max_length=50, blank=True, null=True)
     date_of_birth = models.DateField()
     GENDER_CHOICES = [
@@ -31,15 +36,7 @@ class Profile(models.Model):
     gender = models.CharField(max_length=10, choices=GENDER_CHOICES)
     town = models.CharField(max_length=255, blank=True, null=True)
     phone_number = models.CharField(max_length=50)
-    profile_photo = models.ImageField(upload_to='uploads/profile_photo/', blank=True, null=True)
+    profile_photo = models.ImageField(upload_to='images/profile_photo/', blank=True, null=True)
 
     def __str__(self):
-        return f'{self.surname} , {self.name}'
-
-
-# def create_profile(sender, instance, created, **kwargs):
-# 	if created:
-# 		user_profile = Profile(user=instance)
-# 		user_profile.save()
-#
-# post_save.connect(create_profile, sender=User)
+        return f'{self.last_name} , {self.first_name}'
