55c19c361c1683160824b5963156bf3f9e2024ae
nad-usik <124509955+nad-usik@users.noreply.github.com>
2024-02-14 00:43:31 +0300

    feat: update admin page

--- learning_platform/Users/admin.py
+++ learning_platform/Users/admin.py
@@ -1,28 +1,25 @@
 from django.contrib import admin
 from django.contrib.auth.models import Group
-from django.contrib.auth.admin import UserAdmin
-from .models import CastomUser
+from .models import CustomUser
+# from django.contrib.auth.admin import UserAdmin
 from .models import Profile
-
 
 # Uregister Groups
 admin.site.unregister(Group)
 
-# Mix Profile info into User info
-# class ProfileInline(admin.StackedInline):
-# 	model = Profile
 
-# Extend User Model
-# class UserAdmin(admin.ModelAdmin):
-# 	model = User
-# 	# Just display username fields on admin page
-# 	fields = ["first_name", "last_name", "email"]
-# 	# inlines = [ProfileInline]
-#
-# # Unregister initial User
-# admin.site.unregister(User)
-
-# Reregister CastomUser
-admin.site.register(CastomUser, UserAdmin)
+# Mix Profile info into Users info
+class ProfileInline(admin.StackedInline):
+    model = Profile
 
 
+# Extend Users Model
+class CustomUserAdmin(admin.ModelAdmin):
+    model = CustomUser
+    fields = ["email", "role", "password"]
+    inlines = [ProfileInline]
+    list_display = ("email", "role")
+
+
+# Reregister CustomUser and CustomUserAdmin
+admin.site.register(CustomUser, CustomUserAdmin)
